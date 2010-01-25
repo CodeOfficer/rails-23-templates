352925272380ac46cb89d03dcbafa9ab360f08b4
Russell Jones <spam@codeofficer.com>
2010-01-25 03:52:07 -0500

    tweak

--- basic.rb
+++ basic.rb
@@ -25,36 +25,12 @@ run "rm README"
 run "cp config/database.yml config/database.yml.example"
 run "rm public/index.html"
 run "rm -f public/javascripts/*"
-file 'public/stylesheets/application.css', ''
-file 'public/javascripts/application.js', %q{
-$(function() {
-	// play that funky music white boy ...
-});
-}
+# file 'public/stylesheets/application.css', ''
 
 # INSTALL JQUERY & UI ----------------------------------------------------------
 
 run "curl -L http://ajax.googleapis.com/ajax/libs/jquery/#{JQUERY_VERSION}/jquery.js > public/javascripts/jquery-#{JQUERY_VERSION}.js"
 run "curl -L http://ajax.googleapis.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/jquery-ui.js > public/javascripts/jquery-ui-#{JQUERY_UI_VERSION}.js"
-
-# APP CONFIG & LAYOUT ----------------------------------------------------------
-
-file 'config/config.yml', %q{
-defaults: &DEFAULTS
-  your: mom
-  
-development:
-  domain: localhost:3000
-  <<: *DEFAULTS
-  
-test:
-  domain: test.local
-  <<: *DEFAULTS
-
-production:
-  domain: example.com
-  <<: *DEFAULTS
-}
 
 # INITIALIZERS -----------------------------------------------------------------
 
@@ -104,7 +80,7 @@ plugin 'default_value_for', :git => "git://github.com/FooBarWidget/default_value
 # run("gem bundle") if yes?("Bundle gems ?")
 
 generate("nifty_config")
-generate("nifty_layout")
+generate("nifty_layout", "--haml")
 generate("nifty_authentication")
 generate("rspec")
 generate("cucumber", "--rspec", "--webrat")
@@ -115,30 +91,33 @@ route("map.root :controller => 'home'")
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
 run %{find . -type d -empty | grep -v "vendor" | grep -v ".git" | grep -v "tmp" | xargs -I xxx touch xxx/.gitignore}
 
+# CUSTOMIZATIONS ---------------------------------------------------------------
+
+file 'config/app_config.yml', %q{
+defaults: &DEFAULTS
+  your: mom
+  
+development:
+  domain: localhost:3000
+  <<: *DEFAULTS
+  
+test:
+  domain: test.local
+  <<: *DEFAULTS
+
+production:
+  domain: example.com
+  <<: *DEFAULTS
+}
+
+file 'public/javascripts/application.js', %q{
+$(function() {
+	// play that funky music white boy ...
+});
+}
+
+# FINISH -----------------------------------------------------------------------
+
 git :add => "."
 git :commit => " -m 'Initial commit'"
-
 puts "SUCCESS!"
-
-# # Run a regular expression replacement on a file
-# #
-# # ==== Example
-# #
-# #   gsub_file 'app/controllers/application_controller.rb', /#\s*(filter_parameter_logging :password)/, '\1'
-# #
-# def gsub_file(relative_destination, regexp, *args, &block)
-#   path = destination_path(relative_destination)
-#   content = File.read(path).gsub(regexp, *args, &block)
-#   File.open(path, 'wb') { |file| file.write(content) }
-# end
-# 
-# # Append text to a file
-# #
-# # ==== Example
-# #
-# #   append_file 'config/environments/test.rb', 'config.gem "rspec"'
-# #
-# def append_file(relative_destination, data)
-#   path = destination_path(relative_destination)
-#   File.open(path, 'ab') { |file| file.write(data) }
-# end
